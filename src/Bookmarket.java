58f2867c8f5e895dca52ac05b6fe5d7857d77cee
kimdongju123 <08555@naver.com>
2024-03-12 14:44:15 +0900

    Level 1 finish!

---
+++ src/Bookmarket.java
@@ -0,0 +1,10 @@
+
+public class Bookmarket {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        System.out.println("Welcome to shopping Mall!");
+        System.out.println("Welcome to BookMarket!!");
+	}
+
+}
