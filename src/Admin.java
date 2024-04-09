327cad8f9f1241f13182fe767ce56c173dccbb69
kimdongju123 <08555@naver.com>
2024-04-09 17:22:56 +0900

    finish

---
+++ src/Admin.java
@@ -0,0 +1,14 @@
+public class Admin extends person {
+	private String id ="admin";
+	private String password = "admin1234";
+	
+	public Admin(String name,int phone) {
+		super(name,phone);
+	}
+	public String getId() {
+		return id;
+	}
+	public String getPassword() {
+		return password;
+	}
+}
